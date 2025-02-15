750fc3a6dde39693ec14352c0b924b01f8073fbf
Florian <florianmart2004@gmail.com>
2025-02-15 22:16:15 +0100

    css for add car

--- addCar.php
+++ addCar.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Compagnion App</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/main.css'>
+    <link rel="stylesheet" type='text/css' media='screen' href='./css/cadre.css'>
+    <link rel="stylesheet" type='text/css' media='screen' href='./css/floatingButton.css'>
+    <link rel="stylesheet" type='text/css' media='screen' href='./css/form.css'>
+    <script src='main.js'></script>
+
+    <!-- FONT -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Inline&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
+</head>
+
 <?php
 session_start();
 require "config.php"; // Connexion à la base de données
@@ -22,11 +42,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
-<h2>Ajouter un véhicule</h2>
-<form method="post">
-    <label>Nom du véhicule :</label>
-    <input type="text" name="car_name" required>
-
-    <button type="submit">Ajouter</button>
+<section>
+    <h1>Ajouter un véhicule</h1>
+</section>
+<section class="form">
+    <form method="post">
+        <label>Nom du véhicule :</label>
+        <input type="text" name="car_name" required>
+</section>
+<section class="submitSection">
+    <button type="submit" class="submit">Ajouter</button>
+</section>
 </form>
